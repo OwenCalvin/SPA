2fe38339f907e2bce6bd0f2a91ecf1843abc15f0
Owen Gombas <owen.gombas@gmail.com>
2018-02-13 14:20:35 +0100

    Rename vue

--- vue_spa/src/articles/Pages/Page1.vue
+++ vue_spa/src/articles/Pages/Page1.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="hello">
     <h1>Page1</h1>
+    <img src="../../assets/logo.png" alt="">
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Article1'
+  name: 'Page1'
 }
 </script>
